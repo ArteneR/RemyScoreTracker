4e4e4583c23a087470f98b3164ab1690059414c6
ArteneR <georgevici.ioan@yahoo.com>
2016-07-25 17:04:28 +0300

    Added Name and Color input fields for adding new player

--- public/js/players-page.js
+++ public/js/players-page.js
@@ -1,23 +1,24 @@
 
 (function(global) {
 
+    global.toggleNewPlayerContainerHeight = function(delay) {
+        $('#new-player-container').animate({
+            height: "toggle"
+        }, delay);
+    }
+
+
     showNavBar();
+    toggleNewPlayerContainerHeight(0);
     $("#page-title").html("Players");
 
     $("#back-button").click(function() {
-
         hideNavBar();
         loadPage("start-page");
-
     });
 
     $("#add-player-button").click(function() {
-
-        $('#new-player-container').animate({
-            height: "toggle"
-        }, 500);
-
+        toggleNewPlayerContainerHeight(500);
     });
-
 
 })(this);
